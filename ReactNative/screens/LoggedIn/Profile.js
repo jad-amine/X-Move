c1017dfec6acb23d19f9771c1c9b31526f5650a3
jad amine <jade1992amine@gmail.com>
2022-07-10 15:26:37 +0300

    Front End: Added ImagePicker to send Profile Picture to the backend

--- ReactNative/screens/LoggedIn/Profile.js
+++ ReactNative/screens/LoggedIn/Profile.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useContext, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { UserContext } from "../../contexts/UserContext";
 import * as ImagePicker from "expo-image-picker";
 import { Entypo } from "@expo/vector-icons";
@@ -8,7 +8,7 @@ import { global } from "../../styles/globalStyles";
 
 const Profile = () => {
   const { user } = useContext(UserContext);
-  const info = JSON.stringify(user);
+  const [image, setImage] = useState(null);
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -16,27 +16,56 @@ const Profile = () => {
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
+      base64: true,
     });
+    // console.log(result);
+    if (result.cancelled) return;
+    try {
+      const response = await fetch(
+        "http://10.0.2.2:4000/api/users/addProfilePicture",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            authorization: `Bearer ${user.token}`,
+          },
+          body: JSON.stringify({ base64: result }),
+        }
+      );
+      const data = await response.json();
+      setImage(data.uri);
+
+      console.log(data);
+    } catch (err) {
+      console.log("request error ==>", err);
+    }
   };
   return (
-    <View style={global.profileHeader}>
-      <TouchableOpacity style={global.profilePic} onPress={pickImage}>
-        <FontAwesome
-          style={{ alignSelf: "center" }}
-          name="user"
-          size={50}
-          color="gray"
-        />
-        <Entypo
-          style={{ padding: 5, borderRadius: 20 }}
-          name="camera"
-          size={24}
-          color="black"
-        />
-      </TouchableOpacity>
-      <Text>{user.info.name}</Text>
-      <Text>{user.info.email}</Text>
-      <Text>{user.info.sports}</Text>
+    <View>
+      <View style={global.profileHeader}>
+        <TouchableOpacity style={global.profilePic} onPress={pickImage}>
+          <FontAwesome
+            style={{ alignSelf: "center" }}
+            name="user"
+            size={50}
+            color="gray"
+          />
+          <Entypo
+            style={{ padding: 5, borderRadius: 20 }}
+            name="camera"
+            size={24}
+            color="black"
+          />
+        </TouchableOpacity>
+        <View style={{ flexDirection: "column", marginLeft: 20 }}>
+          <Text>{user.info.name}</Text>
+          <Text>{user.info.email}</Text>
+          <Text>{user.info.sports}</Text>
+        </View>
+      </View>
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   );
 };

0cbef110bdb7de10ed8d8b7cd463f10da3f50560
jad amine <jade1992amine@gmail.com>
2022-07-11 08:41:57 +0300

    Front End: Added Logic to toggle profile pic & add profile pic if not found

--- ReactNative/screens/LoggedIn/Profile.js
+++ ReactNative/screens/LoggedIn/Profile.js
@@ -16,7 +16,7 @@ import * as SecureStore from "expo-secure-store";
 
 const Profile = () => {
   const { user, setUser } = useContext(UserContext);
-  const [image, setImage] = useState(null);
+
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -27,6 +27,13 @@ const Profile = () => {
       base64: true,
     });
     if (result.cancelled) return;
+    setUser({
+      info: {
+        ...user.info,
+        picture: result.base64,
+      },
+      token: user.token,
+    });
     try {
       const response = await fetch(
         "http://10.0.2.2:4000/api/users/addProfilePicture",
@@ -36,21 +43,26 @@ const Profile = () => {
             "Content-type": "application/json",
             authorization: `Bearer ${user.token}`,
           },
-          body: JSON.stringify({ base64: result }),
+          body: JSON.stringify({ base64: result.base64 }),
         }
       );
       const data = await response.json();
-      setImage(data.uri);
-
-      console.log(data);
     } catch (err) {
       console.log("request error ==>", err);
     }
   };
+
   return (
     <View>
       <View style={global.profileHeader}>
-        {!user.info.picture && (
+        {user.info.picture ? (
+          <TouchableOpacity>
+            <Image
+              source={{ uri: `data:image/gif;base64,${user.info.picture}` }}
+              style={{ height: 200, width: 200 }}
+            />
+          </TouchableOpacity>
+        ) : (
           <TouchableOpacity style={global.profilePic} onPress={pickImage}>
             <FontAwesome
               style={{ alignSelf: "center" }}
@@ -66,28 +78,24 @@ const Profile = () => {
             />
           </TouchableOpacity>
         )}
-        {user.info.picture && <Image source={user.info.picture} />}
         <View style={{ flexDirection: "column", marginLeft: 20 }}>
           <Text>{user.info.name}</Text>
           <Text>{user.info.email}</Text>
           <Text>{user.info.sports}</Text>
         </View>
       </View>
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-      )}
+
       <Button
         title="Sign Out"
         onPress={() => {
           try {
-            SecureStore.setItemAsync("token", null);
+            SecureStore.setItemAsync("token", "");
             setUser(null);
           } catch (err) {
             Alert.alert("Unable to logout ");
           }
         }}
       />
-      <Text onPress={() => console.log(user.info.picture)}>Hello</Text>
     </View>
   );
 };

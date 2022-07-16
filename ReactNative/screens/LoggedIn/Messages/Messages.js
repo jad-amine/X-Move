7b01fb19474160d0397babcadfbc3c92af7252d9
jad amine <jade1992amine@gmail.com>
2022-07-16 10:36:55 +0300

    Front End: Added Metro Config File to use Firebase version 9 SDK

--- ReactNative/screens/LoggedIn/Messages/Messages.js
+++ ReactNative/screens/LoggedIn/Messages/Messages.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Chat from "./Chat";
 import Conversations from "./Conversations";
@@ -7,6 +7,9 @@ import {
   collection,
   onSnapshot,
   initializeFirestore,
+  setDoc,
+  doc,
+  addDoc,
 } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
@@ -20,6 +23,7 @@ import {
   appId,
 } from "@env";
 import { MessagesContext } from "../../../contexts/MessagesContext";
+import { UserContext } from "../../../contexts/UserContext";
 
 const firebaseConfig = {
   apiKey: apiKey,
@@ -35,20 +39,29 @@ const Stack = createNativeStackNavigator();
 
 const Messages = ({ route }) => {
   const [allMessages, setAllMessages] = useState(null);
+  const { user } = useContext(UserContext);
   var db;
   var colRef;
   var app;
   useEffect(() => {
     // init firebase app
-    app = initializeApp(firebaseConfig);
+    initializeApp(firebaseConfig);
 
     // init firestore service (connect to db)
-    db = initializeFirestore(app, { experimentalForceLongPolling: true });
-    // db = getFirestore();
+    db = getFirestore();
 
     // collection ref
     colRef = collection(db, "messages");
 
+    addDoc(colRef, {
+      // text: messages,
+      // createdAt: new Date(),
+      _id: user.info._id,
+      user: {
+        _id: "2",
+        name: "Sam",
+      },
+    });
     const unsubCol = onSnapshot(colRef, (snapshot) => {
       // we put q instead of colRef to perform a query
       let messages = [];

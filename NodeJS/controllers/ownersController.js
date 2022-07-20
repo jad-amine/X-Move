af319da1316aaf7da7e58abf53346c4fe3e7d4b8
jad amine <jade1992amine@gmail.com>
2022-07-20 07:19:28 +0300

    Back End: Added Log In route

--- NodeJS/controllers/ownersController.js
+++ NodeJS/controllers/ownersController.js
@@ -35,4 +35,58 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+// Login Owner
+const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user || !req.body.password) {
+    return res.status(401).json({ message: "Invalid Credentials" });
+  }
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) {
+    return res.status(401).json({ message: "Invalid Credentials" });
+  }
+  var token = jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      type: user.admin,
+      sports: user.sports,
+      friends: user.friends,
+    },
+    process.env.TOKEN_SECRET
+  );
+  console.log(user, "logged in");
+  if (user.pictureURL) {
+    const profilepic = fs.readFileSync(`${user.pictureURL}`, {
+      encoding: "utf8",
+      flag: "r",
+    });
+    res.status(200).json({
+      token: token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        type: user.admin,
+        sports: user.sports,
+        friends: user.friends,
+        picture: profilepic,
+      },
+    });
+  } else {
+    res.status(200).json({
+      token: token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        type: user.admin,
+        sports: user.sports,
+        friends: user.friends,
+      },
+    });
+  }
+};
+
+module.exports = { register, login };
--- NodeJS/routes/ownersRoutes.js
+++ NodeJS/routes/ownersRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { register } = require("../controllers/ownersController");
+const { register, login } = require("../controllers/ownersController");
 
 const router = express.Router();
 
 // Register
 router.post("/register", register);
+
+// Login
+router.post("/login", login);
 
 module.exports = router;

d1b6ba14315cb3243524edb9ac05df9c070a31af
jad amine <jade1992amine@gmail.com>
2022-07-24 10:41:34 +0300

    Front End (RN): Added API call to send Friend Request

--- ReactNative/screens/LoggedIn/Home/Sports/PlayerProfile.js
+++ ReactNative/screens/LoggedIn/Home/Sports/PlayerProfile.js
@@ -1,19 +1,38 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import { global } from "../../../../styles/globalStyles";
+import { UserContext } from "../../../../contexts/UserContext";
 
 const PlayerProfile = ({ route }) => {
-  const user = route.params;
+  const { user } = useContext(UserContext);
+  const player = route.params;
+
+  const handlePress = async () => {
+    try {
+      const response = await fetch("http://10.0.2.2:4000/api/users/addFriend", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({ id: player._id }),
+      });
+      const json = await response.json();
+      console.log(json.message);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
       <View style={{ margin: 10, flexDirection: "row" }}>
         <Image
-          source={{ uri: `http://10.0.2.2:4000/` + user.pictureURL }}
+          source={{ uri: `http://10.0.2.2:4000/` + player.pictureURL }}
           style={{ height: 150, width: 150 }}
         />
         <View style={{ padding: 40, flex: 1 }}>
-          <Text style={{ fontSize: 35 }}>{user.name}</Text>
-          <Text style={{ color: "gray" }}>{user.email}</Text>
+          <Text style={{ fontSize: 35 }}>{player.name}</Text>
+          <Text style={{ color: "gray" }}>{player.email}</Text>
         </View>
       </View>
       <View
@@ -24,6 +43,7 @@ const PlayerProfile = ({ route }) => {
         }}
       >
         <TouchableOpacity
+          onPress={handlePress}
           style={{ ...global.PlayerProfileButton, backgroundColor: "#FF4D00" }}
         >
           <Text style={{ ...global.registerText, fontSize: 18 }}>
@@ -31,6 +51,7 @@ const PlayerProfile = ({ route }) => {
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
+          onPress={() => console.log("hi")}
           style={{
             ...global.PlayerProfileButton,
             backgroundColor: "#2C75E2",
@@ -40,6 +61,7 @@ const PlayerProfile = ({ route }) => {
         </TouchableOpacity>
       </View>
       <Text style={global.about}>About</Text>
+      <Text>{player.about}</Text>
     </>
   );
 };

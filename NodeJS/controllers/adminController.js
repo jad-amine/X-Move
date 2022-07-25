08b02a738c60ed8f47e84c059939682f713c2580
jad amine <jade1992amine@gmail.com>
2022-07-25 13:57:18 +0300

    Back End: Added admin login route

--- NodeJS/controllers/adminController.js
+++ NodeJS/controllers/adminController.js
@@ -1,10 +1,47 @@
-const User = require("../models/userModel");
 const mongoose = require("mongoose");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+
+// Admin Login
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user || !req.body.password || user.admin != 1) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+    var token = jwt.sign(
+      {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        type: user.admin,
+      },
+      process.env.TOKEN_SECRET
+    );
+    res.status(200).json({
+      token: token,
+      user: {
+        _id: user._id,
+        name: user.name,
+      },
+    });
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
+};
 
 // Get all users
-const getAllUsers = async (req, res, next) => {
+const getAllUsers = async (req, res) => {
   const users = await User.find();
   res.status(200).json(users);
 };
 
-module.exports = { getAllUsers };
+module.exports = { getAllUsers, login };
--- NodeJS/routes/adminRoutes.js
+++ NodeJS/routes/adminRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { authAdmin } = require("../middlewares/adminMiddleware");
-const { getAllUsers } = require("../controllers/adminController");
+const { getAllUsers, login } = require("../controllers/adminController");
 
 const router = express.Router();
+
+// Login
+router.post("/login", login);
 
 // Get all users
 router.get("/getUsers", authAdmin, getAllUsers);

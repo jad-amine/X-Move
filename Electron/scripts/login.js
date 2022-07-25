a8849573a1b4c3ddfc1d1d028866ee7871b4b478
jad amine <jade1992amine@gmail.com>
2022-07-25 15:17:30 +0300

    Front End (Electron): Added Welcome page

--- Electron/scripts/login.js
+++ Electron/scripts/login.js
@@ -2,6 +2,7 @@
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const button = document.querySelector("#log-in");
+const error_message = document.querySelector("#error-message");
 
 //Send data and store response (token and name) in localstorage
 button.addEventListener("click", async () => {
@@ -16,8 +17,9 @@ button.addEventListener("click", async () => {
     console.log(data);
     localStorage.setItem("name", data.user.name);
     localStorage.setItem("token", data.token);
-    // location.href = "./pages/welcome.html";
+    location.href = "./pages/welcome.html";
   } catch (error) {
+    error_message.innerHTML = "Invalid Credentials";
     console.log(error.message);
   }
 });

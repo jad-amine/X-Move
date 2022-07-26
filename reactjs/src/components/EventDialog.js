f95a5d0fe5f61df68d2f0f3df375773aa5d5b370
jad amine <jade1992amine@gmail.com>
2022-07-26 09:36:39 +0300

    Front End (React): Added API call to reschedule events

--- reactjs/src/components/EventDialog.js
+++ reactjs/src/components/EventDialog.js
@@ -14,26 +14,61 @@ export default function EventDialog({
   user,
   setAllEvents,
   allEvents,
+  reschedule,
+  rescheduledEvent,
+  setEventToDelete,
 }) {
   const handleClose = () => {
+    setEventToDelete(false);
     setShowEventDialog(false);
   };
 
   const handleDelete = async () => {
-    console.log(eventToDelete);
-    setShowEventDialog(false);
     try {
       const { data } = await API.get("deleteReservation/" + eventToDelete.id, {
         headers: {
           Authorization: `Bearer ${user.token}`,
         },
       });
-      setShowEventDialog(false);
-      setAllEvents(allEvents.filter((event) => event.id !== eventToDelete.id));
+      if (data === "Removed") {
+        setShowEventDialog(false);
+        setAllEvents(
+          allEvents.filter((event) => event.id !== eventToDelete.id)
+        );
+      }
     } catch (error) {
       console.log(error, error.message);
     }
   };
+
+  const handleReschedule = async () => {
+    try {
+      const { data } = await API.post(
+        "rescheduleReservation/" + rescheduledEvent.event.id,
+        { rescheduledEvent },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      if (data === "Reservation Rescheduled !!") {
+        const updatedEvents = allEvents.filter(
+          (event) => event.id !== rescheduledEvent.event.id
+        );
+        updatedEvents.push({
+          ...rescheduledEvent.event,
+          start: rescheduledEvent.start,
+          end: rescheduledEvent.end,
+        });
+        setAllEvents(updatedEvents);
+        setShowEventDialog(false);
+      }
+    } catch (error) {
+      console.log(error, error.message);
+    }
+  };
+
   if (eventToDelete) {
     return (
       <div>
@@ -50,6 +85,35 @@ export default function EventDialog({
             <Button onClick={handleClose}>Cancel</Button>
             <Button style={{ color: "red" }} onClick={handleDelete}>
               Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+  if (reschedule) {
+    return (
+      <div>
+        <Dialog open={showEventDialog} onClose={handleClose}>
+          <DialogTitle>
+            Reschedule {rescheduledEvent.event.player} Event
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              <p>From:</p>{" "}
+              {rescheduledEvent.event.start.toString().slice(0, 21)}
+              <p style={{ margin: 20 }}></p>
+              <p>Till:</p> {rescheduledEvent.event.end.toString().slice(0, 21)}
+              <hr />
+              <p>From:</p> {rescheduledEvent.start.toString().slice(0, 21)}
+              <p style={{ margin: 20 }}></p>
+              <p>Till:</p> {rescheduledEvent.end.toString().slice(0, 21)}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button style={{ color: "green" }} onClick={handleReschedule}>
+              Reschedule
             </Button>
           </DialogActions>
         </Dialog>

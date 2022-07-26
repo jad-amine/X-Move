ba3ff758a9ad45c2e69103afc7a91f45622dc7b1
jad amine <jade1992amine@gmail.com>
2022-07-26 05:52:22 +0300

    Front End (React): Added Calendar Dialog component

--- reactjs/src/pages/OwnerCalendar.js
+++ reactjs/src/pages/OwnerCalendar.js
@@ -1,17 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import API from "../api";
 import { UserContext } from "../contexts/UserContext";
 import moment from "moment";
+import CalendarDialog from "../components/CalendarDialog";
 
 const localizer = momentLocalizer(moment);
 
 export default function OwnerCalendar() {
   const [newEvent, setNewEvent] = useState({ player: "", start: "", end: "" });
   const [allEvents, setAllEvents] = useState([]);
+  const [showDialog, setShowDialog] = useState(false);
   const { user } = useContext(UserContext);
 
   useEffect(() => {
@@ -26,10 +27,12 @@ export default function OwnerCalendar() {
         const fetchedEvents = data.reservations.map((event) => {
           return {
             ...event,
+            title: `${event.player} reservation`,
             start: new Date(event.start),
             end: new Date(event.end),
           };
         });
+        console.log(fetchedEvents);
         setAllEvents([...fetchedEvents]);
       } catch (error) {
         alert("Reservations couldn't be fetched !!");
@@ -39,17 +42,23 @@ export default function OwnerCalendar() {
     getReservations();
   }, []);
 
+  const handleSelect = (e) => {
+    setNewEvent({
+      ...newEvent,
+      start: e.start,
+      end: e.end,
+    });
+    setShowDialog(true);
+  };
+
   const handleAddEvent = async () => {
-    if (
-      newEvent.player === "" ||
-      newEvent.start === "" ||
-      newEvent.end === ""
-    ) {
+    setShowDialog(false);
+    if (newEvent.player === "") {
       alert("Please Fill all the fields");
       return;
     }
     try {
-      const { data } = await API.post(
+      await API.post(
         "addGame/",
         { newEvent: newEvent, propertyID: user.info.property._id },
         {
@@ -69,43 +78,22 @@ export default function OwnerCalendar() {
     <div className="calendar-form">
       <h1 className="align-center">Calendar</h1>
       <Calendar
+        popup={true}
+        onSelectSlot={handleSelect}
+        selectable={true}
         localizer={localizer}
         startAccessor="start"
         endAccessor="end"
         events={allEvents}
         style={{ height: 500, margin: "50px" }}
       />
-      <div className="add-event">
-        <h2>Add New Event</h2>
-        <div className="date-picker">
-          <input
-            type="text"
-            placeholder="Player "
-            style={{ width: "20%", marginRight: "10px" }}
-            value={newEvent.player}
-            onChange={(e) =>
-              setNewEvent({ ...newEvent, player: e.target.value })
-            }
-          />
-          <DatePicker
-            placeholderText="Start Date"
-            selected={newEvent.start}
-            showTimeSelect
-            dateFormat="Pp"
-            onChange={(start) => setNewEvent({ ...newEvent, start })}
-          />
-          <DatePicker
-            placeholderText="End Date"
-            selected={newEvent.end}
-            showTimeSelect
-            dateFormat="Pp"
-            onChange={(end) => setNewEvent({ ...newEvent, end })}
-          />
-        </div>
-        <button id="addEvent-button" onClick={handleAddEvent}>
-          Add Event
-        </button>
-      </div>
+      <CalendarDialog
+        setShowDialog={setShowDialog}
+        showDialog={showDialog}
+        newEvent={newEvent}
+        setNewEvent={setNewEvent}
+        handleAddEvent={handleAddEvent}
+      />
     </div>
   );
 }

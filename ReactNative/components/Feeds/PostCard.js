dac0f72ad1b2dcfe2d923b0cbd1727b722f93f9a
jad amine <jade1992amine@gmail.com>
2022-07-27 16:48:41 +0300

    Front End (RN): Styled Post Card

--- ReactNative/components/Feeds/PostCard.js
+++ ReactNative/components/Feeds/PostCard.js
@@ -1,7 +1,12 @@
 import * as React from "react";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { TextInput } from "react-native-paper";
 
 const PostCard = ({ post }) => {
+  const [addComment, setAddComment] = React.useState(false);
+  const [comment, setComment] = React.useState("");
   const LeftContent = (props) =>
     post.picture ? (
       <Avatar.Image
@@ -18,21 +23,32 @@ const PostCard = ({ post }) => {
       />
     );
   return (
-    <Card>
-      <Card.Title
-        title={post.name}
-        subtitle="Card Subtitle"
-        left={LeftContent}
-      />
+    <Card style={{ marginVertical: 15, paddingVertical: 5 }}>
+      <Card.Title title={post.name} left={LeftContent} />
       <Card.Cover source={{ uri: `http://10.0.2.2:4000/` + post.picture }} />
+      <Card.Actions style={{ marginBottom: -15 }}>
+        <Button>
+          <AntDesign name="hearto" size={24} color="black" />
+        </Button>
+        <Button onPress={() => setAddComment(!addComment)}>
+          <Feather name="message-circle" size={26} color="#333" />
+        </Button>
+        <Button>
+          <Feather name="bookmark" size={24} color="black" />
+        </Button>
+      </Card.Actions>
       <Card.Content>
         <Title>{post.caption}</Title>
         <Paragraph>{post.createdAt.toString()}</Paragraph>
+        {addComment && (
+          <TextInput
+            style={{ margin: 5, height: 45 }}
+            label="Add comment.."
+            value={comment}
+            onChangeText={setComment}
+          />
+        )}
       </Card.Content>
-      <Card.Actions>
-        <Button>Cancel</Button>
-        <Button>Ok</Button>
-      </Card.Actions>
     </Card>
   );
 };

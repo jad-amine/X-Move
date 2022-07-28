29fb5e817382421f49f02cc9316e6d25ecc611c0
jad amine <jade1992amine@gmail.com>
2022-07-28 12:53:27 +0300

    Front End (Electron): Added API call to authenticate admin on App start

--- Electron/src/App.js
+++ Electron/src/App.js
@@ -1,5 +1,39 @@
+import React, { useEffect, useState } from "react";
+import LandingPage from "./windows/LandingPage";
+import API from "./api";
+
 function App() {
-  return <div className="App">hi</div>;
+  const [applicationData, setApplicationData] = useState();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+    else {
+      const authAdmin = async () => {
+        try {
+          const { data } = await API.post(
+            "authAdmin/",
+            { mission: "Auth Admin" },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (data.message === "Validated") {
+            setApplicationData({ players: data.players, fields: data.fields });
+          }
+        } catch (error) {
+          setApplicationData(null);
+          console.log(error.message, error);
+        }
+      };
+      authAdmin();
+    }
+  }, []);
+
+  if (!applicationData) return <LandingPage />;
+  return <div className="App"></div>;
 }
 
 export default App;

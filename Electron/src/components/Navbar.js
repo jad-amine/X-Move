df933754a4d510cc9a93ef4b3312d25be3998b7f
jad amine <jade1992amine@gmail.com>
2022-07-28 15:53:07 +0300

    Front End (Electron): Added Navbar component

---
+++ Electron/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import logo from "../assets/logo1.png";
+import { FaInstagram, FaFacebookSquare, FaTwitter } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
+// import { UserContext } from "../contexts/UserContext";
+
+function Navbar({ setShowDrawer }) {
+  return (
+    <div className="top-nav">
+      <div className="hamburger-menu">
+        <FiMenu
+          cursor="pointer"
+          size={30}
+          color="black"
+          onClick={() => {
+            setShowDrawer(true);
+          }}
+        />
+        <img
+          className="to-nav-img"
+          src={logo}
+          height={60}
+          width={90}
+          alt="logo"
+        />
+      </div>
+      <ul>
+        <li>
+          <FaFacebookSquare size={25} color="black" />
+        </li>
+        <li>
+          <FaInstagram size={25} color="black" />
+        </li>
+        <li>
+          <FaTwitter size={25} color="black" />
+        </li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;

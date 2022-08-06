9e37512076c11520c257edbea8078a40c0088620
jad amine <jade1992amine@gmail.com>
2022-08-06 06:47:59 +0300

    Front End (RN): Moving title option to stack navigatore screenOptions

--- X-Move-reactnative/screens/LoggedIn/Home/StackNavigator.js
+++ X-Move-reactnative/screens/LoggedIn/Home/StackNavigator.js
@@ -1,64 +1,62 @@
 // Utilities
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// Pages
+
+// Screens
 import Home from "./Home";
 import Ball from "./Sports/Ball";
-import Cycling from "./Sports/Cycling";
-import Entertainments from "./Sports/Entertainments";
-import Fitness from "./Sports/Fitness";
-import Players from "./Sports/Players";
 import Water from "./Sports/Water";
 import Winter from "./Sports/Winter";
+import Cycling from "./Sports/Cycling";
+import Fitness from "./Sports/Fitness";
+import Players from "./Sports/Players";
+import Entertainments from "./Sports/Entertainments";
 import PlayerProfile from "./Sports/PlayerProfile";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerBackTitleVisible: false }}>
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerTitleAlign: "center",
+      }}
+    >
       <Stack.Screen
         options={{ headerShown: false }}
         name="Home"
         component={Home}
       />
+      <Stack.Screen name="Fitness" component={Fitness} />
+      <Stack.Screen name="Cycling" component={Cycling} />
       <Stack.Screen
-        options={{ headerTitleAlign: "center" }}
-        name="Fitness"
-        component={Fitness}
-      />
-      <Stack.Screen
-        options={{ headerTitleAlign: "center" }}
-        name="Cycling"
-        component={Cycling}
-      />
-      <Stack.Screen
-        options={{ headerTitle: "Water Sports", headerTitleAlign: "center" }}
+        options={{ headerTitle: "Water Sports" }}
         name="Water"
         component={Water}
       />
       <Stack.Screen
-        options={{ headerTitle: "Winter Sports", headerTitleAlign: "center" }}
+        options={{ headerTitle: "Winter Sports" }}
         name="Winter"
         component={Winter}
       />
       <Stack.Screen
-        options={{ headerTitle: "Ball Sports", headerTitleAlign: "center" }}
+        options={{ headerTitle: "Ball Sports" }}
         name="Ball"
         component={Ball}
       />
       <Stack.Screen
-        options={{ headerTitle: "Entertainment", headerTitleAlign: "center" }}
+        options={{ headerTitle: "Entertainment" }}
         name="Entertainment"
         component={Entertainments}
       />
       <Stack.Screen
-        options={{ headerTitle: "Players", headerTitleAlign: "center" }}
+        options={{ headerTitle: "Players" }}
         name="Players"
         component={Players}
       />
       <Stack.Screen
-        options={{ headerTitle: "Player Profile", headerTitleAlign: "center" }}
+        options={{ headerTitle: "Player Profile" }}
         name="PlayerProfile"
         component={PlayerProfile}
       />

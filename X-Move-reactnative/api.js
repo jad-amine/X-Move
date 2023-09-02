4f2857265fa7f98aa86882a64c747fe1b0fab18f
Jad Joseph Amine <85282660+jad-amine@users.noreply.github.com>
2023-09-02 16:23:04 +0300

    Delete X-Move-reactnative directory

--- X-Move-reactnative/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: `http://192.168.1.3:4000/api/users/`,
-});
--- X-Move-reactnative/contexts/MessagesContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext, useReducer } from "react";
-
-export const MessagesContext = createContext(null);
